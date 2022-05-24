0334d5a75f5bc76005d8936608c841e65dab59dc
Mateus Araújo <mathsilvaa20@hotmail.com>
2022-05-24 02:49:46 -0300

    feat: added unit tests in helpers

---
+++ spec/infrastrucure/helpers/http.spec.ts
@@ -0,0 +1,24 @@
+import Http from '../../../src/infrastructure/helpers/http';
+
+import HttpProtocol from '../../../src/infrastructure/protocols/http-protocol';
+
+describe('[Infrastructure / Helpers] - Http', (): void => {
+  test('[ok] - Should return httpResponse', async () => {
+    const body = { message: 'test' };
+    const response: HttpProtocol = Http.ok(body);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe(body);
+  });
+
+  test('[serverError] - Should return httpResponse', async () => {
+    const error = new Error('error');
+    const response: HttpProtocol = Http.serverError(error);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({
+      error: 'Oops! An error has occurred!',
+      message: error.message,
+    });
+  });
+});
