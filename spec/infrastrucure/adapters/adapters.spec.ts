ecb08f35c4956efa3f48340679397d321f1dcf0f
Mateus Araújo <mathsilvaa20@hotmail.com>
2022-05-24 02:23:40 -0300

    feat: added unit tests in adapters

---
+++ spec/infrastrucure/adapters/adapters.spec.ts
@@ -0,0 +1,25 @@
+import Http from '../../../src/infrastructure/helpers/http';
+import RouterAdapter from '../../../src/infrastructure/adapters/router-adapter';
+
+import HttpProtocol from '../../../src/infrastructure/protocols/http-protocol';
+
+describe('[Infrastructure / Adapters] - Router Adapter', (): void => {
+	test('Should return httpReponse', async () => {
+		class MockRouter {
+			public static async route(): Promise<HttpProtocol> {
+				return Http.ok({});
+			}
+		}
+
+		const adapt = RouterAdapter.adapt(MockRouter.route);
+
+		const json = jest.fn();
+		const status = jest.fn().mockReturnValueOnce({
+			json,
+		});
+
+		await adapt({}, { status });
+
+		expect(status).toHaveBeenCalledWith(200);
+	});
+});
