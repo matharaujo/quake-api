97612cb9fa6613d7c935ab803755a911eb59a325
Mateus Araújo <mathsilvaa20@hotmail.com>
2022-05-24 00:15:06 -0300

    fix: return express response

--- src/infrastructure/adapters/router-adapter.ts
+++ src/infrastructure/adapters/router-adapter.ts
@@ -2,10 +2,13 @@ import express from 'express';
 
 class RouterAdapter {
   public static adapt(router: () => any): any {
-    return async (request: express.Request, response: express.Response) => {
+    return async (
+      request: express.Request,
+      response: express.Response
+    ): Promise<express.Response> => {
       const httpResponse = await router();
 
-      response.status(httpResponse.statusCode).json(httpResponse.body);
+      return response.status(httpResponse.statusCode).json(httpResponse.body);
     };
   }
 }
