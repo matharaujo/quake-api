c13dba9c27107a7d4325250f191b10b0f2ac5db9
Mateus Araújo <mathsilvaa20@hotmail.com>
2022-05-24 19:17:32 -0300

    feat: added non existent route message

--- src/infrastructure/configurations/application.ts
+++ src/infrastructure/configurations/application.ts
@@ -11,6 +11,16 @@ export default (): void => {
   initializeMiddlewares(application);
   initializeRouter(application);
 
+  application.use(
+    '*',
+    (request: express.Request, response: express.Response) => {
+      response.status(404).send({
+        error: 'Oops! An error has occurred!',
+        message: `Apparently this route ${request.originalUrl} does not exist!`,
+      });
+    }
+  );
+
   const port = process.env.PORT_API || 8000;
   application.listen(port, () =>
     console.log(`Server running at http://localhost:${port}/`)
